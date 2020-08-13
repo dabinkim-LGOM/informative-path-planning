7d567cc94e63c2102e94b9bd3a23b31254d0fa03
dabinkim-LGOM <dabin404@snu.ac.kr>
2020-08-14 00:35:06 +0900

    1

--- python_scripts/scripts/baseline/global_BO.py
+++ python_scripts/scripts/baseline/global_BO.py
@@ -3,8 +3,10 @@ Baseline algorithm of Continuous Belief Tree Search (CBTS)
 (IROS18 Continuous State-Action-Observation POMDPs
 for Trajectory Planning with Bayesian Optimisation)
 Global inner optimization of Bayesian Optimization for continuous action selection(dynamic sampling)
+Optimization is based on particle swarm optimization. 
 '''
-
+import numpy as np
+import 
 class Baseline(object):
     def __init__(self, size):
         self.size = size

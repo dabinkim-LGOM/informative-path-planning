8c15f76341883799eddef6f6ba39a550f12266e4
dabkinkim-LGOM <dabin404@snu.ac.kr>
2020-09-21 16:13:05 +0900

    a

--- python_scripts/scripts/CBTS.py
+++ python_scripts/scripts/CBTS.py
@@ -33,6 +33,37 @@ from MCTS import *
 Baseline code for Continuous Belief Tree Search using PSO as global optimization for bayesian optimization 
 IROS18 "Continuous State-Action-Observation POMDPs for Trajectory Planning with Bayesian Optimization" 
 '''
+
+class Node_C(object):
+    def __init__(self, pose, parent, name, action = None, dense_path = None, zvals = None):
+        self.pose = pose
+        self.name = name
+        self.zvals = zvals 
+        self.reward = 0.0
+        self.nqueries = 0
+
+        self.parent = parent 
+        self.children = children 
+
+        self.action = action 
+        self.dense_path = dense_path 
+        if parent is None:
+            self.depth = 0
+        else:
+            self.depth = parent.depth +1 
+    
+    def add_children(self, child_node):
+        if self.children is None:
+            self.children = [] 
+        self.children.append(child_node)
+    
+    def remove_children(self, child_node):
+        if child_node in self.children: self.children.remove(child_node)
+    
+    def print_self(self):
+        print(self.name)
+
+
 class Tree_C(object):
     def __init__(self, ranges, obstacle_world, f_rew, f_aqu,  belief, pose, t, max_depth, param):
         self.ranges = ranges 
@@ -56,6 +87,156 @@ class Tree_C(object):
     def get_best_child(self):
         return self.root.children[np.argmax([node.nqueries for node in self.root.children])]
 
+    def backprop(self, leaf_node, reward):
+        if leaf_node.parent is None:
+            leaf_node.nqueries += 1
+            leaf_node.reward += reward
+            #print "Calling backprop on:",
+            #leaf_node.print_self()
+            #print "nqueries:", leaf_node.nqueries, "reward:", leaf_node.reward
+            return
+        else:
+            leaf_node.nqueries += 1
+            leaf_node.reward += reward
+            #print "Calling backprop on:",
+            #leaf_node.print_self()
+            #print "nqueries:", leaf_node.nqueries, "reward:", leaf_node.reward
+            self.backprop(leaf_node.parent, reward)
+            return
+    
+    
+    def rollout(self, current_node, belief):
+        cur_depth = current_node.depth
+        tmp_depth = cur_depth 
+        cur_pose = current_node.pose 
+        cumul_reward = 0.0 
+        while cur_depth <= tmp_depth + self.max_rollout_depth:
+            actions, dense_paths = self.path_generator.get_path_set(cur_pose)
+            
+            selected_action =random.choice(actions) 
+
+            if(len(selected_action) == 0):
+                return cumul_reward
+            # print(selected_action)
+            cur_pose = selected_action[-1]
+            
+            obs = np.array(cur_pose)
+            xobs = np.vstack([obs[0], obs[1]]).T
+
+            if self.f_rew == 'mes' or self.f_rew == 'maxs-mes':
+                r = self.aquisition_function(time = self.t, xvals = xobs, robot_model = belief, param = self.param)
+            elif self.f_rew == 'exp_improve':
+                r = self.aquisition_function(time = self.t, xvals = xobs, robot_model = belief, param = self.param)
+            elif self.f_rew == 'naive':
+                # param = sample_max_vals(belief, t=self.t, nK=int(self.param[0]))
+                r = self.aquisition_function(time = self.t, xvals = xobs, robot_model = belief, param = self.param)#(param, self.param[1]))
+            elif self.f_rew == 'naive_value':
+                r = self.aquisition_function(time = self.t, xvals = xobs, robot_model = belief, param = self.param)
+            else:
+                r = self.aquisition_function(time = self.t, xvals = xobs, robot_model = belief)
+            
+            cumul_reward += r
+            cur_depth += 1
+        return cumul_reward 
+
+    def get_reward(self, sequence):
+        '''Evaluate the sequence to get the reward, defined by the percentage of entropy reduction'''
+        # The process is iterated until the last node of the rollout sequence is reached 
+        # and the total information gain is determined by subtracting the entropies 
+        # of the initial and final belief space.
+        # reward = infogain / Hinit (joint entropy of current state of the mission)
+        sim_world = self.belief
+        samples = []
+        obs = []
+        for seq in sequence:
+            samples.append(seq)
+        obs = np.array(samples)
+        # print "Get reward samples: ", samples 
+            # print obs.size
+        if(len(obs)==0):
+            print("Observation set is empty", current_node)
+                # continue 
+        xobs = np.vstack([obs[:,0], obs[:,1]]).T
+        # obs = list(chain.from_iterable(samples))
+        # print "Obs", xobs
+        if(self.aquisition_function==aqlib.mves ):
+            #TODO: Fix the paramter setting. This leads to wrong MES acquisition function computation.
+            # maxes, locs, funcs = sample_max_vals(robot_model=sim_world, t=t, nK = 3, nFeatures = 200, visualize = False, obstacles=obslib.FreeWorld(), f_rew='mes'): 
+            return self.aquisition_function(time = self.t, xvals = xobs, param= (self.max_val, self.max_locs, self.target), robot_model = sim_world)
+        else:
+            return self.aquisition_function(time = self.t, xvals = xobs, robot_model = sim_world)
+
+    def get_next_child(self, current_node):
+        vals = {}
+        # e_d = 0.5 * (1.0 - (3.0/10.0*(self.max_depth - current_node.depth)))
+        e_d = 0.5 * (1.0 - (3.0/(10.0*(self.max_depth - current_node.depth))))
+        for i, child in enumerate(current_node.children):
+            #print "Considering child:", child.name, "with queries:", child.nqueries
+            if child.nqueries == 0:
+                return child
+            # vals[child] = child.reward/float(child.nqueries) + self.c * np.sqrt((float(current_node.nqueries) ** e_d)/float(child.nqueries)) 
+            vals[child] = child.reward/float(child.nqueries) + self.c * np.sqrt(np.log(float(current_node.nqueries))/float(child.nqueries)) 
+        # Return the max node, or a random node if the value is equal
+        return random.choice([key for key in vals.keys() if vals[key] == max(vals.values())])
+        
+    def build_action_children(self, parent):
+        # print(self.path_generator.get_path_set(parent.pose))
+        # print(self.path_generator)
+        actions, dense_paths = self.path_generator.get_path_set(parent.pose)
+
+        # print "Actions: ", actions
+        free_actions, free_dense_paths = self.collision_check(actions, dense_paths)
+        # print "Action set: ", free_actions 
+        # actions = self.path_generator.get_path_set(parent.pose)
+        # dense_paths = [0]
+        if len(actions) == 0:
+            print("No actions!")
+            return
+        
+        # print "Creating children for:", parent.name
+        for i, action in enumerate(free_actions.keys()):
+            # print "Action:", parent.name + '_action' + str(i)
+            if len(free_actions[action])!=0:
+                # print free_actions[action]
+                parent.add_children(Node(pose = parent.pose, 
+                                        parent = parent, 
+                                        name = parent.name + '_action' + str(i), 
+                                        action = free_actions[action], 
+                                        dense_path = free_dense_paths[action],
+                                        zvals = None))
+                # print("Adding next child: ", parent.name + '_action' + str(i))
+
+    def collision_check(self, path_dict, path_dense_dict):
+        free_paths = {}
+        dense_free_paths = {}
+        for key,path in path_dict.items():
+            is_collision = 0
+            for pt in path:
+                if(self.obstacle_world.in_obstacle(pt, 3.0)):
+                    is_collision = 1
+            if(is_collision == 0):
+                free_paths[key] = path
+                dense_free_paths[key] = path_dense_dict[key]
+        
+        return free_paths, dense_free_paths
+    
+
+    def print_tree(self):
+        counter = self.print_helper(self.root)
+        print("# nodes in tree:", counter)
+
+    def print_helper(self, cur_node):
+        if cur_node.children is None:
+            #cur_node.print_self()
+            #print cur_node.name
+            return 1
+        else:
+            #cur_node.print_self()
+            #print "\n"
+            counter = 0
+            for child in cur_node.children:
+                counter += self.print_helper(child)
+            return counter
 
 class CBTS(MCTS):
     def __init__(self, ranges, obstacle_world, computation_budget, belief, initial_pose, max_depth, max_rollout_depth, frontier_size,
@@ -75,7 +256,7 @@ class CBTS(MCTS):
         self.x_bound, self.y_bound = 10.0, 10.0 
         self.num_action = frontier_size # Number of actions 
 
-        self.gp_kern = GPy.kern.RBF(input_dim = self.dim, lengthscale = lengthscale, variance = variance) 
+        self.gp_kern = GPy.kern.RBF(input_dim = self.dim, lengthscale = lengthscale, variance = variance) # kernel of GP for reward 
 
     def get_actions(self):
         # self.tree = Tree(self.ranges, self.obstacle_world, self.f_rew, self.acquisition_function, self.belief, self.initial_pose, self.path_generator, self.time, 
